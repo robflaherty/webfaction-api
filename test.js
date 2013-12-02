e6a52f6df6f3c64acc21b878652d1493a7457ab3
Rob Flaherty <rob@ravelrumba.com>
2013-12-02 17:26:18 -0500

    adding queue helper

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 var Webfaction = require('./lib/webfaction');
 
 var webfaction = new Webfaction();
-
+/*
 webfaction.login(function(result) {
   
   //console.log(result);
@@ -13,10 +13,10 @@ webfaction.login(function(result) {
       console.log(result);
     });
 
-  /*
   webfaction.listMachines(function(result){
     console.log(result);
   });
-  */
 
 });
+
+  */
